79f11faac79ddf648dab2bb13a5de19b5dfb82a4
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-01-08 23:39:26 +0530

    Added the session

---
+++ sourcecode/Session.java
@@ -0,0 +1,12 @@
+package lms_project;
+
+public class Session {
+   static int id;
+   static String name;
+   static String campus;
+   static String Batch_id;
+   static String Hostel;
+  public static void main(String[] args) {
+	System.out.println(name);
+}
+}
