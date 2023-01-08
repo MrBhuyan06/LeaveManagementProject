2ee3e7f5c4d1102199e66edc1af76be9e7823ad2
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2023-01-08 23:38:05 +0530

    added the faculty session

---
+++ sourcecode/SessionFaculty.java
@@ -0,0 +1,14 @@
+package lms_project;
+
+public class SessionFaculty {
+
+	
+	  static int id;
+	   static String name;
+	   static String campus;
+	   static String Mentor_Id;
+//	   static String Hostel;
+	   public static void main(String[] args) {
+		System.out.println(Mentor_Id);
+	}
+}
